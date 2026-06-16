a130938faaae85a4db6db3ff68e55f30468a40f7
Alejandro <klycito21@gmail.com>
2026-06-16 14:50:01 -0400

    Reparados los CORS en main.ts para produccion

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,12 +12,14 @@ async function bootstrap() {
       transform: true
     })
   );
+
+  // 🌐 CONFIGURACIÓN DE CORS REPARADA PARA PRODUCCIÓN
   app.enableCors({
-    origin: 'http://localhost:3000',  // URL de tu React (Vite)
+    origin: '*',  
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,  // Si usas cookies/sesiones
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
